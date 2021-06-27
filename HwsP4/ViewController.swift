2ba2687520194a7d9fafb87dcf7deb79b9cb183d
Terry <terrykuo501@gmail.com>
2021-06-28 01:04:46 +0800

    finish

--- HwsP4/ViewController.swift
+++ HwsP4/ViewController.swift
@@ -6,14 +6,93 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    private var webView: WKWebView!
+    private var progressView: UIProgressView!
+    private let url = URL(string: "https://www.apple.com.tw")!
+    private let websites = ["apple.com", "hackingwithswift.com"]
+    
+    lazy var openButton = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+    lazy var spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+    lazy var refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+    lazy var progressButton = UIBarButtonItem(customView: progressView)
+    
+    override func loadView() { //gets called before viewDidLoad
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        
+        toolbarItems = [progressButton, spacer, refresh]
+        navigationController?.isToolbarHidden = false
+        navigationItem.rightBarButtonItem =  openButton
+        
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil) //!!!
+        
+        
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
+    @objc private func openTapped() {
+        let ac = UIAlertController(title: "Open Page...", message: nil, preferredStyle: .actionSheet)
+        
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        ac.popoverPresentationController?.barButtonItem = openButton
+        present(ac, animated: true, completion: nil)
     }
 
+    private func openPage(action: UIAlertAction) {
+        if let urlTitle = action.title {
+            if let urll = URL(string: "https://" + urlTitle) {
+                webView.load(URLRequest(url: urll))
+            }
+        }
+    }
+    
+    
 
 }
 
+
+//MARK: - Extension
+
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let urlll = navigationAction.request.url
+        
+        if let host = urlll?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        
+        decisionHandler(.cancel)
+    }
+}
